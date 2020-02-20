e87b9070f9334d5198611ba5b501c758bf12ef7f
Tim DiLauro <tim.di.lauro@gmail.com>
2020-02-20 18:21:55 -0500

    accept command-line arguments

--- wikipedia_episodes.py
+++ wikipedia_episodes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import asyncio
 from wikipedia_series import WikipediaSeries
 
@@ -8,41 +9,31 @@ from wikipedia_series import WikipediaSeries
 Scrape episode data from Wikipedia "episode" pages
 '''
 
-def main(show_season_urls):
-    series = [WikipediaSeries.from_url(url) for url in show_season_urls]
+def main(urls):
+    series = [WikipediaSeries.from_url(url) for url in urls]
     return series
 
-async def async_main(show_season_urls):
-    pending = [asyncio.create_task(WikipediaSeries.async_from_url(url)) for url in show_season_urls]
+
+async def async_main(urls):
+    pending = [asyncio.create_task(WikipediaSeries.async_from_url(url)) for url in urls]
     series = await asyncio.gather(*pending, return_exceptions=True)
     return series
 
 
 if __name__ == "__main__":
-    show_season_urls = [
-        'https://en.wikipedia.org/wiki/Good_Omens_(TV_series)',
-        'https://en.wikipedia.org/wiki/Star_Trek:_Discovery_(season_1)',
-        'https://en.wikipedia.org/wiki/The_Big_Bang_Theory_(season_1)',
-        'https://en.wikipedia.org/wiki/The_Big_Bang_Theory_(season_2)',
-        'https://en.wikipedia.org/wiki/The_Big_Bang_Theory_(season_3)',
-        'https://en.wikipedia.org/wiki/The_Big_Bang_Theory_(season_4)',
-        'https://en.wikipedia.org/wiki/The_Big_Bang_Theory_(season_5)',
-        'https://en.wikipedia.org/wiki/The_Big_Bang_Theory_(season_6)',
-        'https://en.wikipedia.org/wiki/The_Big_Bang_Theory_(season_7)',
-        'https://en.wikipedia.org/wiki/The_Big_Bang_Theory_(season_8)',
-        'https://en.wikipedia.org/wiki/The_Big_Bang_Theory_(season_9)',
-        'https://en.wikipedia.org/wiki/The_Big_Bang_Theory_(season_10)',
-        'https://en.wikipedia.org/wiki/The_Big_Bang_Theory_(season_11)',
-        'https://en.wikipedia.org/wiki/The_Big_Bang_Theory_(season_12)',
-    ]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--sync', action='store_true', help='Use synchronous, rather than asynchronous, retrieval')
+    parser.add_argument('urls', nargs='+')
+    args = parser.parse_args()
 
-    # async/await approach
-    async_event_loop = asyncio.get_event_loop()
-    series = async_event_loop.run_until_complete(async_main(show_season_urls))
-    async_event_loop.stop()
+    urls = args.urls
 
-    # standard (synchronous) approach
-    # series = main(show_season_urls)
+    if args.sync:
+        series = main(args.urls)
+    else:
+        async_event_loop = asyncio.get_event_loop()
+        series = async_event_loop.run_until_complete(async_main(args.urls))
+        async_event_loop.stop()
 
     for s in series:
         print(s.as_json())
